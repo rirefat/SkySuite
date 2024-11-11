7f87ddf904e29c084db8d7752187194da882316f
rirefat <rirefat.official@gmail.com>
2024-11-12 01:41:42 +0600

    implement hotel details page

--- components/hotel/HotelCard.jsx
+++ components/hotel/HotelCard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const HotelCard = () => {
     return (
         <div className="flex gap-6 border border-gray/20 p-4 rounded-md">
-            <Image src="/image-1.png" className="max-h-[162px] max-w-[240px]" alt="" width={150} height={50} />
+            <Image src="/image-1.png" className="max-h-[162px] max-w-[240px]" alt="" width={200} height={50} />
             <div className="flex-1">
                 <h2 className="font-bold text-lg">Effotel By Sayaji Jaipur</h2>
                 <p>📍 Kolkata</p>
