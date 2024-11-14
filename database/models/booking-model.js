39c7764e428f9e9c5003aa695263cc393b075eae
rirefat <rirefat.official@gmail.com>
2024-11-15 03:18:37 +0600

    done booking model and schema

--- database/models/booking-model.js
+++ database/models/booking-model.js
@@ -1,7 +1,21 @@
 import mongoose, { Schema } from "mongoose";
 
 const bookingSchema = new Schema({
-
+    hotelId: {
+        required: true,
+        type: ObjectId
+    },
+    userId: {
+        required: true,
+        type: ObjectId
+    },
+    checkin: {
+        required: true,
+        type: String
+    },
+    checkout: {
+        required: true,
+        type: String
+    },
 });
-
 export const BookingModel = mongoose.models.bookings ?? mongoose.model('bookings', bookingSchema);
