ecc21bfb6c73eac3148e88e1049dd215c7b0a14d
rirefat <rirefat.official@gmail.com>
2024-11-15 05:43:53 +0600

    implement google provider login

--- components/auth/SocialLogins.jsx
+++ components/auth/SocialLogins.jsx
@@ -1,6 +1,13 @@
+'use client'
+import { signIn } from "next-auth/react"
 import Image from "next/image";
 
 const SocialLogins = () => {
+
+  const handleGoogleAuth = () => {
+    signIn('google', { callbackUrl: `${process.env.ROOT_PAGE}/bookings` });
+  }
+
   return (
     <>
       <div className="text-center text-xs text-gray-500">or Signup with</div>
@@ -9,7 +16,10 @@ const SocialLogins = () => {
           <Image src="/fb.png" alt="facebook" width={25} height={24} />
           <span>Facebook</span>
         </button>
-        <button className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center">
+        <button
+          onClick={handleGoogleAuth}
+          className=" w-full mt-4 py-2 border-gray-600/30 border rounded-md flex items-center gap-2 justify-center"
+        >
           <Image src="/google.png" alt="google" width={25} height={24} />
           <span>Google</span>
         </button>
