701777933481cce617af5dfe38eeef8b22979184
rirefat <rirefat.official@gmail.com>
2024-11-15 19:29:56 +0600

    done storing users data after authentication

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Facebook from "next-auth/providers/facebook";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import client from "./lib/mongoClientPromise";
 
 export const {
     handlers: { GET, POST },
@@ -8,6 +10,7 @@ export const {
     signOut,
     auth
 } = NextAuth({
+    adapter: MongoDBAdapter(client),
     providers: [
         Google({
             clientId: process.env.AUTH_GOOGLE_ID,
