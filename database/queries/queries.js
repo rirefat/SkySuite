70f59ba0181277f60ce177e5cba69e56864e4edd
rirefat <rirefat.official@gmail.com>
2024-11-18 21:14:50 +0600

    done queries for getting hotel reviews and ratings

--- database/queries/queries.js
+++ database/queries/queries.js
@@ -1,9 +1,21 @@
 import { replaceMongoIdInArray } from "@/utils";
 import { HotelsModel } from "../models/hotels-model"
+import { RatingsModel } from "../models/ratings-model";
+import { ReviewModel } from "../models/reviews-model";
 
 export const getAllHotels = async () => {
     const allHotels = await HotelsModel.find()
         .select((["thumbNailUrl", "name", "highRate", "lowRate", "city", "propertyCategory"]))
         .lean();
     return replaceMongoIdInArray(allHotels);
+}
+
+export const getHotelRatings = async (hotelId) => {
+    const ratings = await RatingsModel.find({ hotelId: hotelId }).lean();
+    return replaceMongoIdInArray(ratings);
+}
+
+export const getHotelReviews = async (hotelId) => {
+    const reviews = await ReviewModel.find({ hotelId: hotelId }).lean();
+    return replaceMongoIdInArray(reviews);
 }
